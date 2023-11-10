76fcada1aa90a7f129663a26fd9a69118e9c5a50
AlexDeft <aleaparecido9888@gmail.com>
2023-11-10 12:13:08 -0300

    Update condicionais.js

--- Javascript/Desafios/if e switch/condicionais.js
+++ Javascript/Desafios/if e switch/condicionais.js
@@ -78,7 +78,7 @@ else {
     console.log("É triangulo Escaleno")
 }
 
-//###################################################################################################################################################//
+//##############################################################################################################################################################################################################################################################################################################################################################//
 // 05 - Verificação de Ano Bissexto
 
 // Desenvolva um programa que receba um ano como entrada e verifica se ele é bissexto ou não. Um ano bissexto é aquele que é divisível por 4, exceto por anos que são divisíveis por 100, a menos que sejam divisíveis por 400. Imprima "Ano bissexto" ou "Não é um ano bissexto" com base na entrada. (2000, 1996)
