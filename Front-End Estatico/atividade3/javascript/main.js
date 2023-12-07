4397467c529e6bc313ea73dc3da1c932600f8382
AlexDeft <aleaparecido9888@gmail.com>
2023-12-06 22:15:30 -0300

    Update main.js

--- Front-End Estatico/atividade3/javascript/main.js
+++ Front-End Estatico/atividade3/javascript/main.js
@@ -1,3 +1,4 @@
 import { Cafe } from "./Cafe.js";
 
-console.log(new Cafe())
+console.log(new Cafe(1, "Espresso", 1.99, ["café, agua"]))
+console.log(new Cafe(2,"Cappuccino", 2.99, ["café, agua, canela"]))
